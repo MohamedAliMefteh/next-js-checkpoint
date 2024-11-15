3459fd4c3898bc2bc1359698a92e532a9e492b54
MohamedAliMefteh <mohamedalimefteh.fl@gmail.com>
2024-11-15 15:42:08 +0100

    fc

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['example.com'], // Add your image domains here if needed
+    },
+  };
+  
+  export default nextConfig;
